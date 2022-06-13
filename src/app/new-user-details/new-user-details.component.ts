223ad50c6527a09c84308ad8d5567c104cdf3462
PandharkarSayali <97429105+PandharkarSayali@users.noreply.github.com>
2022-06-13 17:34:02 +0530

    Update new-user-details.component.ts

--- src/app/new-user-details/new-user-details.component.ts
+++ src/app/new-user-details/new-user-details.component.ts
@@ -18,11 +18,7 @@ export class NewUserDetailsComponent implements OnInit {
     console.log("abcd");
     console.log("abcd")
      console.log(abcd);
-     console.log("abcd");
-     console.log(2);
-     console.log(2);
-        console.log(abcd);
-        console.log(abcd);
+
    
   
   }
