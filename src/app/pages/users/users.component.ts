45ae8fbeb8ea2c552c2d71c8270bed2ce139d971
PandharkarSayali <97429105+PandharkarSayali@users.noreply.github.com>
2023-02-14 14:48:39 +0530

    pppp

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-
 export class UsersComponent implements OnInit {
   constructor() { }
   ngOnInit(): void {  
